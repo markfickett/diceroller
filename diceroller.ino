41b7cb707902b9493bbfbe98916620da1e7bea23
Mark Fickett <mark.fickett@gmail.com>
2015-11-14 15:39:16 -0500

    Make number of rolls configurable: ground pin 4 for 1k rolls, leave NC / high for 3000.

--- diceroller.ino
+++ diceroller.ino
@@ -1,10 +1,13 @@
 #define PIN_STATUS_LED 0
 #define PIN_SERVO 2
 #define PIN_PICTURE 1
+#define PIN_COUNT 4
 
 // Empirically, the shape of a histogram or roll results for a d20 seems to
 // settle at/above 2k rolls, so stopping at 3k should produce good results.
-#define AUTO_STOP_COUNT 3000
+// For a d6 it settles around 500-1000.
+#define AUTO_STOP_COUNT_LONG 3000
+#define AUTO_STOP_COUNT_SHORT 1000
 
 // Usable range is around [-30, 200].
 #define ANGLE_SHAKE -30
@@ -19,13 +22,17 @@
 
 uint16_t servoMicros;
 uint16_t rollCount;
+uint16_t maxRollCount;
 
 void setup() {
   pinMode(PIN_SERVO, OUTPUT);
   setServoAngle(ANGLE_UPRIGHT);
   pinMode(PIN_PICTURE, OUTPUT);
   pinMode(PIN_STATUS_LED, OUTPUT);
+  pinMode(PIN_COUNT, INPUT_PULLUP);
 
+  maxRollCount = digitalRead(PIN_COUNT) ?
+      AUTO_STOP_COUNT_LONG : AUTO_STOP_COUNT_SHORT;
   rollCount = 0;
 }
 
@@ -49,7 +56,7 @@ void loop() {
   digitalWrite(PIN_PICTURE, LOW);
   digitalWrite(PIN_STATUS_LED, LOW);
 
-  if (++rollCount > AUTO_STOP_COUNT) {
+  if (++rollCount > maxRollCount) {
     cli(); // stop interrupts
     while (true);
   }
